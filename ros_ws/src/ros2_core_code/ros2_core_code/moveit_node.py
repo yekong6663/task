a709b87dde098aa084f6e4428382a3badab5eb21
yozora <744311194@qq.com>
2025-02-23 12:17:46 +0800

    导入库

---
+++ ros_ws/src/ros2_core_code/ros2_core_code/moveit_node.py
@@ -0,0 +1,14 @@
+"""
+@file moveit_node.py
+@mainpage rclpy库 geomtry_msgs/msg下的Port类型 moveit2库中的PlanningCompoent类 
+@brief 基于moveit2以python代码实现从一个点移动到另一个点
+@authon yekong6663
+@version 1.0
+@date 25-2-22
+@note ...
+"""
+import moveit2 
+import moveit2.moveit_py
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Pose
