9850ee8822dde02233f1a9d7a8cb2a598a73264b
yozora <744311194@qq.com>
2025-02-23 12:18:42 +0800

    创建类,并初始化属性

--- ros_ws/src/ros2_core_code/ros2_core_code/moveit_node.py
+++ ros_ws/src/ros2_core_code/ros2_core_code/moveit_node.py
@@ -12,3 +12,20 @@ import moveit2.moveit_py
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose
+"""
+@brief MoveNode_t类,用于实现核心功能
+@detail 描述详细信息
+"""
+class MoveNode_t(Node):
+    """
+    @brief 类初始化
+    @detail 初始化MoveGroupCommander类对象,目标位置Pose对象
+    """ 
+    def __init__(self,target_pose:Pose):
+        super().__init__("move_node")
+        self.moveit=moveit2.moveit_py("move_node")
+        self.group=self.moveit.get__planning_component("whole")
+        self.target_pose=target_pose
+        self.target_pose.orientation.w=1.0
+    
+
